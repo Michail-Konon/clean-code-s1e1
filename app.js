7a47da11edb373a3e3876cea775d4e18db6d8928
michail-konon <greateyuri@gamil.com>
2023-04-22 20:35:53 +0300

    fix(extended-1.2): fix missing alt properties

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="btn delete";
     deleteButtonImg.src="./remove.svg";
     deleteButtonImg.className="item-img"
+    deleteButtonImg.alt="remove icon";
     deleteButton.appendChild(deleteButtonImg);
 
 
